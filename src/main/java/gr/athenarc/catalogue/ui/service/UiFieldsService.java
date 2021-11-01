5947879b3d5716ce34547b1168e7e0d823182b8e
Konstantinos Spyrou <spyroukon@gmail.com>
2021-11-01 15:10:10 +0200

    changed method order

--- src/main/java/gr/athenarc/catalogue/ui/service/UiFieldsService.java
+++ src/main/java/gr/athenarc/catalogue/ui/service/UiFieldsService.java
@@ -3,64 +3,13 @@ package gr.athenarc.catalogue.ui.service;
 import gr.athenarc.catalogue.ui.domain.*;
 
 import javax.xml.bind.annotation.XmlElement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public interface UiFieldsService {
-
-    default List<UiField> createFields(String className, String parent) throws ClassNotFoundException {
-        List<UiField> fields = new LinkedList<>();
-        Class<?> clazz = Class.forName(className);
-
-
-        if (clazz.getSuperclass().getName().startsWith("gr.athenarc")) {
-//        if (clazz.getSuperclass().getTypeName().length() > 1) {
-            String name = clazz.getGenericSuperclass().getTypeName();
-            if (name.contains("<")) {
-                name = name.substring(name.indexOf("<")+1, name.indexOf(">"));
-            }
-            List<UiField> subfields = createFields(name, name);
-            fields.addAll(subfields);
-        }
-
-        if (clazz.getSuperclass().getName().startsWith("gr.athenarc")) {
-            String name = clazz.getSuperclass().getName()/*.replaceFirst(".*\\.", "")*/;
-            List<UiField> subfields = createFields(name, name);
-            fields.addAll(subfields);
-        }
-
-        java.lang.reflect.Field[] classFields = clazz.getDeclaredFields();
-        for (java.lang.reflect.Field field : classFields) {
-            UiField uiField = new UiField();
-
-//            field.setAccessible(true);
-            uiField.setName(field.getName());
-            uiField.setParent(parent);
-
-            XmlElement annotation = field.getAnnotation(XmlElement.class);
-
-            if (annotation != null) {
-                uiField.getForm().setMandatory(!annotation.nillable());
-
-                String type = field.getType().getName();
-
-                if (Collection.class.isAssignableFrom(field.getType())) {
-                    uiField.setMultiplicity(true);
-                    type = field.getGenericType().getTypeName();
-                    type = type.substring(type.indexOf("<")+1, type.indexOf(">"));
-                }
-                String typeName = type.replaceFirst(".*\\.", "").replaceAll("[<>]", "");
-                uiField.setType(typeName);
-
-                if (type.startsWith("gr.athenarc")) {
-                    List<UiField> subfields = createFields(type, field.getName());
-                    fields.addAll(subfields);
-                }
-            }
-            fields.add(uiField);
-        }
-        return fields;
-    }
 
     UiField getField(int id);
 
@@ -133,4 +82,57 @@ public interface UiFieldsService {
         return groupedFieldsList;
     }
 
+    default List<UiField> createFields(String className, String parent) throws ClassNotFoundException {
+        List<UiField> fields = new LinkedList<>();
+        Class<?> clazz = Class.forName(className);
+
+
+        if (clazz.getSuperclass().getName().startsWith("gr.athenarc")) {
+//        if (clazz.getSuperclass().getTypeName().length() > 1) {
+            String name = clazz.getGenericSuperclass().getTypeName();
+            if (name.contains("<")) {
+                name = name.substring(name.indexOf("<") + 1, name.indexOf(">"));
+            }
+            List<UiField> subfields = createFields(name, name);
+            fields.addAll(subfields);
+        }
+
+        if (clazz.getSuperclass().getName().startsWith("gr.athenarc")) {
+            String name = clazz.getSuperclass().getName()/*.replaceFirst(".*\\.", "")*/;
+            List<UiField> subfields = createFields(name, name);
+            fields.addAll(subfields);
+        }
+
+        java.lang.reflect.Field[] classFields = clazz.getDeclaredFields();
+        for (java.lang.reflect.Field field : classFields) {
+            UiField uiField = new UiField();
+
+//            field.setAccessible(true);
+            uiField.setName(field.getName());
+            uiField.setParent(parent);
+
+            XmlElement annotation = field.getAnnotation(XmlElement.class);
+
+            if (annotation != null) {
+                uiField.getForm().setMandatory(!annotation.nillable());
+
+                String type = field.getType().getName();
+
+                if (Collection.class.isAssignableFrom(field.getType())) {
+                    uiField.setMultiplicity(true);
+                    type = field.getGenericType().getTypeName();
+                    type = type.substring(type.indexOf("<") + 1, type.indexOf(">"));
+                }
+                String typeName = type.replaceFirst(".*\\.", "").replaceAll("[<>]", "");
+                uiField.setType(typeName);
+
+                if (type.startsWith("gr.athenarc")) {
+                    List<UiField> subfields = createFields(type, field.getName());
+                    fields.addAll(subfields);
+                }
+            }
+            fields.add(uiField);
+        }
+        return fields;
+    }
 }
