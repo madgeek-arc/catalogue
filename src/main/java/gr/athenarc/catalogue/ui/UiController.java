5996f6197db8f135f01e92f2725358176dcc8c3f
Konstantinos Spyrou <spyroukon@gmail.com>
2021-11-03 17:25:23 +0200

    changed ui controller path

--- src/main/java/gr/athenarc/catalogue/ui/UiController.java
+++ src/main/java/gr/athenarc/catalogue/ui/UiController.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("ui")
 public class UiController {
 
     private final UiFieldsService uiFieldsService;
