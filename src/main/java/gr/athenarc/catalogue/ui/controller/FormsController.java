87585981bc7953a31a01cd14d8328238dba77d67
Konstantinos Spyrou <spyroukon@gmail.com>
2021-12-07 20:20:52 +0200

    added 'forms' prefix to methods

--- src/main/java/gr/athenarc/catalogue/ui/controller/FormsController.java
+++ src/main/java/gr/athenarc/catalogue/ui/controller/FormsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping()
+@RequestMapping("forms")
 public class FormsController {
 
     private final FormsService formsService;
