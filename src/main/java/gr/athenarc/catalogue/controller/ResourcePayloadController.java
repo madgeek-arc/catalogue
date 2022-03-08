b6d53648e85b6f5e73ad30b764ae903ccbc6c5a6
Konstantinos Spyrou <spyroukon@gmail.com>
2022-03-08 16:55:29 +0200

    added correct contentType to responses

--- src/main/java/gr/athenarc/catalogue/controller/ResourcePayloadController.java
+++ src/main/java/gr/athenarc/catalogue/controller/ResourcePayloadController.java
@@ -1,8 +1,12 @@
 package gr.athenarc.catalogue.controller;
 
+import eu.openminted.registry.core.dao.ResourceTypeDao;
+import eu.openminted.registry.core.domain.ResourceType;
 import gr.athenarc.catalogue.service.ResourcePayloadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,28 +15,44 @@ import org.springframework.web.bind.annotation.*;
 public class ResourcePayloadController {
 
     private final ResourcePayloadService resourcePayloadService;
+    private final ResourceTypeDao resourceTypeDao;
 
     @Autowired
-    public ResourcePayloadController(ResourcePayloadService resourcePayloadService) {
+    public ResourcePayloadController(ResourcePayloadService resourcePayloadService,
+                                     ResourceTypeDao resourceTypeDao) {
         this.resourcePayloadService = resourcePayloadService;
+        this.resourceTypeDao = resourceTypeDao;
     }
 
     @GetMapping("{id}")
     public ResponseEntity<String> get(@RequestParam("resourceType") String resourceType, @PathVariable("id") String id) {
         String payload = resourcePayloadService.getRaw(resourceType, id);
-        return new ResponseEntity<>(payload, HttpStatus.OK);
+        return new ResponseEntity<>(payload, createContentType(resourceType), HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<String> createResource(@RequestParam("resourceType") String resourceType,
-                                                @RequestBody String resource) {
-        return new ResponseEntity<>(resourcePayloadService.addRaw(resourceType, resource), HttpStatus.OK);
+                                                 @RequestBody String resource) {
+        return new ResponseEntity<>(resourcePayloadService.addRaw(resourceType, resource), createContentType(resourceType), HttpStatus.OK);
     }
 
     @PutMapping("{id}")
     public <T> ResponseEntity<?> update(@PathVariable("id") String id,
                                         @RequestParam("resourceType") String resourceType,
                                         @RequestBody T resource) throws NoSuchFieldException {
-        return new ResponseEntity<>(resourcePayloadService.updateRaw(resourceType, id, (String) resource), HttpStatus.OK);
+        return new ResponseEntity<>(resourcePayloadService.updateRaw(resourceType, id, (String) resource), createContentType(resourceType), HttpStatus.OK);
+    }
+
+    private HttpHeaders createContentType(String resourceTypeName) {
+        ResourceType resourceType = resourceTypeDao.getResourceType(resourceTypeName);
+        HttpHeaders headers = new HttpHeaders();
+        if (resourceType.getPayloadType().equals("xml")) {
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE);
+        } else if (resourceType.getPayloadType().equals("json")) {
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        } else {
+            headers.add(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE);
+        }
+        return headers;
     }
 }
