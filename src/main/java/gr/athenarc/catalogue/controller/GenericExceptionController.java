87a350be0e1ea1a5f586e728202daaa503a906f9
Konstantinos Spyrou <spyroukon@gmail.com>
2022-12-21 11:24:56 +0200

    improved controller advice

--- src/main/java/gr/athenarc/catalogue/controller/GenericExceptionController.java
+++ src/main/java/gr/athenarc/catalogue/controller/GenericExceptionController.java
@@ -2,15 +2,18 @@ package gr.athenarc.catalogue.controller;
 
 import eu.openminted.registry.core.controllers.GenericController;
 import gr.athenarc.catalogue.exception.ResourceAlreadyExistsException;
+import gr.athenarc.catalogue.exception.ResourceException;
 import gr.athenarc.catalogue.exception.ResourceNotFoundException;
 import gr.athenarc.catalogue.exception.ServerError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,19 +22,36 @@ public class GenericExceptionController extends GenericController {
 
     private static final Logger logger = LoggerFactory.getLogger(GenericExceptionController.class);
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseBody
-    ServerError handleNotFound(HttpServletRequest req, Exception ex) {
-        logger.info(ex.getMessage(), ex);
-        return new ServerError(HttpStatus.NOT_FOUND, req, ex);
-    }
-
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler(ResourceAlreadyExistsException.class)
-    @ResponseBody
-    ServerError handleAlreadyExists(HttpServletRequest req, Exception ex) {
-        logger.info(ex.getMessage(), ex);
-        return new ServerError(HttpStatus.CONFLICT, req, ex);
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<ServerError> handleException(HttpServletRequest req, Exception ex) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        if (ex instanceof ResourceException) {
+            logger.info(ex.getMessage(), ex);
+            status = ((ResourceException) ex).getStatus();
+        } else if (ex instanceof HttpClientErrorException) {
+            logger.info(ex.getMessage(), ex);
+            status = ((HttpClientErrorException) ex).getStatusCode();
+        } else if (ex instanceof AccessDeniedException) {
+            logger.info(ex.getMessage());
+            logger.debug(ex.getMessage(), ex);
+            status = HttpStatus.FORBIDDEN;
+        } else if (ex instanceof InsufficientAuthenticationException) {
+            logger.info(ex.getMessage());
+            logger.debug(ex.getMessage(), ex);
+            status = HttpStatus.UNAUTHORIZED;
+        } else if (ex instanceof ResourceAlreadyExistsException) {
+            logger.info(ex.getMessage());
+            logger.debug(ex.getMessage(), ex);
+            status = HttpStatus.CONFLICT;
+        } else if (ex instanceof ResourceNotFoundException || ex instanceof eu.openminted.registry.core.exception.ResourceNotFoundException) {
+            logger.info(ex.getMessage());
+            logger.debug(ex.getMessage(), ex);
+            status = HttpStatus.NOT_FOUND;
+        } else {
+            logger.error(ex.getMessage(), ex);
+        }
+        return ResponseEntity
+                .status(status)
+                .body(new ServerError(status, req, ex));
     }
 }
