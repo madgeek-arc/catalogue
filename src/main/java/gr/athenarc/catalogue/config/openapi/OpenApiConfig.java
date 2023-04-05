7e860323088b8e958b4a04a105d8446617d71f45
Konstantinos Spyrou <spyroukon@gmail.com>
2023-04-05 15:57:05 +0300

    implemented default openapi config using properties

---
+++ src/main/java/gr/athenarc/catalogue/config/openapi/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package gr.athenarc.catalogue.config.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class OpenApiConfig {
+
+    private final OpenApiProperties properties;
+
+    public OpenApiConfig(OpenApiProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @Primary
+    public OpenAPI defaultOpenAPI() {
+        OpenAPI openAPI = new OpenAPI();
+        if (properties != null) {
+            if (properties.getServer() != null) {
+                openAPI.addServersItem(properties.getServer());
+            }
+            if (properties.getInfo() != null) {
+                openAPI.info(properties.getInfo());
+            }
+        }
+        return openAPI;
+    }
+}
---
+++ src/main/java/gr/athenarc/catalogue/config/openapi/OpenApiProperties.java
@@ -0,0 +1,30 @@
+package gr.athenarc.catalogue.config.openapi;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "openapi")
+public class OpenApiProperties {
+
+    Server server;
+    Info info;
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public Info getInfo() {
+        return info;
+    }
+
+    public void setInfo(Info info) {
+        this.info = info;
+    }
+}
