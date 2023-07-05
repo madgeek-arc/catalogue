75faa0597251cec3b0ca01625df2bcf2bda87563
Konstantinos Spyrou <spyroukon@gmail.com>
2023-07-05 17:12:20 +0300

    updated registry version and fixed Model in swagger

--- src/main/java/gr/athenarc/catalogue/CatalogueApplication.java
+++ src/main/java/gr/athenarc/catalogue/CatalogueApplication.java
@@ -2,10 +2,13 @@ package gr.athenarc.catalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
+@SpringBootApplication(exclude = {BatchAutoConfiguration.class})
 public class CatalogueApplication {
 
     public static void main(String[] args) {
